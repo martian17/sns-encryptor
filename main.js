2b2e5e603c582438ce0704731f3cefe9bbc69ce2
martian17 <yakumodanshi@gmail.com>
2022-11-02 01:00:05 +0900

    add locale support

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let body = new ELEM(document.body);
 
 
-let URL = new (function(){
+let SearchBar = new (function(){
     let params = new URLSearchParams(window.location.search);
     this.params = params;
     let pathname =  window.location.pathname;
@@ -39,10 +39,6 @@ let URL = new (function(){
     }
 })();
 
-let getLocale = function(){
-    return URL.query.lang || "EN";
-}
-
 let newarr = function(n){
     let arr = [];
     for(let i = 0; i < n; i++){
@@ -68,6 +64,9 @@ const Events = function(){
 };
 
 class Selector extends ELEM{
+    selected = false;
+    value = 0;
+    options = [];
     constructor(...options){
         super("span","class:selector;");
         let that = this;
@@ -75,26 +74,28 @@ class Selector extends ELEM{
         this.on = bus.on.bind(bus);
         this.emit = bus.emit.bind(bus);
         
-        let selected = false;
-        let value = 0;
-        
         for(let i = 0; i < options.length; i++){
             let ostr = options[i];
             let e = this.add("span",0,ostr,ostr === options[0] ? "":"border-left:solid #888 1px;");
             e.i = i;
+            that.options[i] = e;
             e.on("click",function(){
-                if(selected !== false && value === this.i)return;
-                if(selected){
-                    selected.e.classList.remove("selected");
-                }
-                selected = this;
-                selected.e.classList.add("selected");
-                value = this.i;
-                that.emit("input",value);
+                that.selectIndex(this.i);
             }.bind(e));
         }
     }
-}
+    selectIndex(i,silent){
+        let e = this.options[i];
+        if(this.selected !== false && this.value === i)return;
+        if(this.selected){
+            this.selected.e.classList.remove("selected");
+        }
+        this.selected = e;
+        this.selected.e.classList.add("selected");
+        this.value = i;
+        if(!silent)this.emit("input",this.value);
+    }
+};
 
 class UISection extends ELEM{
     constructor(){
@@ -153,7 +154,8 @@ class TextArea extends ELEM{
 };
 
 
-//actual contents with the locale applied
+locale = 0;
+//actual contents with the locale
 
 class SenderGUI extends ELEM{
     constructor(){
@@ -161,7 +163,11 @@ class SenderGUI extends ELEM{
         //get public key from the receiver
         let s1 = this.add(new UISection);
         s1.show();
-        s1.add("h3",0,"2. 下に相手から貰った公開鍵を貼ってください");
+        s1.add("h3",0,[
+            "2. Wait for the other person to send you their public key, and "+
+            "paste it below",
+            "2. 下に相手から貰った公開鍵を貼ってください"
+        ][locale]);
         s1.input = s1.add(new TextArea);
         s1.dialogue = s1.add(new DialogueBanner);
         let pubkey;
@@ -170,41 +176,71 @@ class SenderGUI extends ELEM{
             try{
                 pubkey = await import_pubkey(val);
             }catch(err){
-                s1.dialogue.warn("公開鍵のフォーマットが正しくありません。");
+                s1.dialogue.warn([
+                    "The public key is malformed",
+                    "公開鍵のフォーマットが正しくありません。"
+                ][locale]);
                 s2.hide();
                 s3.hide();
                 pubkey = null;
                 return;
             }
-            s1.dialogue.success("公開鍵を認識しました");
+            s1.dialogue.success([
+                "Successfully recognized the public key",
+                "公開鍵を認識しました"
+            ][locale]);
             s2.show();
         });
         //enter the message, and click encrypt
         let s2 = this.add(new UISection);
-        s2.add("h3",0,"3. 暗号化するメッセージを入力してください");
+        s2.add("h3",0,[
+            "3. Please enter the message to be encrypted below",
+            "3. 暗号化するメッセージを入力してください"
+        ][locale]);
         s2.input = s2.add(new TextArea);
-        s2.btn = s2.add("div","class:btn;","暗号化する");
+        s2.btn = s2.add("div","class:btn;",[
+            "Encrypt",
+            "暗号化する"
+        ][locale]);
         s2.dialogue = s2.add(new DialogueBanner);
         s2.btn.on("click",async ()=>{
             if(!pubkey){
-                s2.dialogue.warn("公開鍵を入力してください");
+                s2.dialogue.warn([
+                    "Please enter the public key first",
+                    "公開鍵を入力してください"
+                ][locale]);
                 return;
             }
-            s2.dialogue.success("暗号化処理中");
+            s2.dialogue.success([
+                "Encrypting...",
+                "暗号化処理中..."
+            ][locale]);
             let val = s2.input.e.value;
             s3.output.e.value = await encrypt(val,pubkey);
             s3.show();
-            s2.dialogue.success("暗号化処理が完了しました");
+            s2.dialogue.success([
+                "Encryption successfully completed",
+                "暗号化処理が完了しました"
+            ][locale]);
         });
         //send the message to the receiver
         let s3 = this.add(new UISection);
-        s3.add("h3",0,"4. 暗号化されたメッセージをコピーして相手に送ってください。これで送信側全ステップ完了です。");
+        s3.add("h3",0,[
+            "4. Please copy the encrypted message below and send it to the receiver. This is the last step for the sender.",
+            "4. 暗号化されたメッセージをコピーして相手に送ってください。これで送信側全ステップ完了です。",
+        ][locale]);
         s3.output = s3.add(new TextArea(true));
-        s3.btn = s3.add("div","class:btn;","クリップボードにコピー");
+        s3.btn = s3.add("div","class:btn;",[
+            "Copy text to the clipboard",
+            "クリップボードにコピー"
+        ][locale]);
         s3.dialogue = s3.add(new DialogueBanner);
         s3.btn.on("click",()=>{
             navigator.clipboard.writeText(s3.output.e.value);
-            s3.dialogue.success("テキストをコピーしました。");
+            s3.dialogue.success([
+                "Text copied to the clipboard",
+                "テキストをコピーしました。"
+            ][locale]);
         });
     }
 }
@@ -214,71 +250,135 @@ class ReceiverGUI extends ELEM{
         super("div",0,0,"display:none;");
         let s1 = this.add(new UISection);
         s1.show();
-        s1.add("h3",0,"2. 下に表示される公開鍵をコピーして相手に送って下さい(毎回ランダムに生成されます)");
+        s1.add("h3",0,[
+            "2. Please copy the public key below and send it to the other person (The keys will be randomly generated every time the page re-freshes)",
+            "2. 下に表示される公開鍵をコピーして相手に送って下さい(毎回ランダムに生成されます)"
+        ][locale]);
         s1.output = s1.add(new TextArea(true));
         let keyPair;
         const generate = async function(){
             s2.hide();
-            s1.dialogue.success("鍵ペアを生成中...");
+            s1.dialogue.success([
+                "Generating a key pair...",
+                "鍵ペアを生成中..."
+            ][locale]);
             keyPair = await generateKeyPair();
             s1.output.e.value = await export_pubkey(keyPair.publicKey);
-            s1.dialogue.success("鍵ペア生成完了");
+            s1.dialogue.success([
+                "Successfully generated a key pair",
+                "鍵ペア生成完了"
+            ][locale]);
             s2.show();
         }
-        s1.cpybtn = s1.add("div","class:btn","クリップボードにコピー");
+        s1.cpybtn = s1.add("div","class:btn",[
+            "Copy text to the clipboard",
+            "クリップボードにコピー"
+        ][locale]);
         s1.cpybtn.on("click",()=>{
             navigator.clipboard.writeText(s1.output.e.value);
-            s1.dialogue.success("テキストをコピーしました。");
+            s1.dialogue.success([
+                "Text copied to the clipboard",
+                "テキストをコピーしました。"
+            ][locale]);
         });
-        s1.genbtn = s1.add("div","class:btn","再生成↻");
+        s1.genbtn = s1.add("div","class:btn",[
+            "Generate again↻",
+            "再生成↻"
+        ][locale]);
         s1.genbtn.on("click",generate);
         s1.dialogue = s1.add(new DialogueBanner);
         let s2 = this.add(new UISection);
-        s2.add("h3",0,"3. 送信者から届いた暗号メッセージを貼って下さい");
+        s2.add("h3",0,[
+            "3. Please paste the encrypted message below",
+            "3. 送信者から届いた暗号メッセージを貼って下さい"
+        ][locale]);
         s2.input = s2.add(new TextArea);
         s2.input.on("input",async ()=>{
             if(!keyPair.privateKey){
-                s2.dialogue.warn("秘密鍵の読み取りに失敗しました。ステップ1で鍵を再生成してからやり直してください。");
+                s2.dialogue.warn([
+                    "Failed to read the private key. Please try again from the step 1 by re-generating keys.",
+                    "秘密鍵の読み取りに失敗しました。ステップ1で鍵を再生成してからやり直してください。"
+                ][locale]);
             }
             let val = s2.input.e.value;
             let res;
             try{
                 res = await decrypt(val,keyPair.privateKey);
             }catch(err){
-                s2.dialogue.warn("暗号のフォーマットが正しくないか暗号化に使われた公開鍵が違います。");
+                s2.dialogue.warn([
+                    "Malformed ciphertext or wrong public key used for encryption.",
+                    "暗号のフォーマットが正しくないか暗号化に使われた公開鍵が違います。"
+                ][locale]);
                 return;
             }
-            s2.dialogue.success("復号に成功しました。");
+            s2.dialogue.success([
+                "Successfully decrypted the contents",
+                "復号に成功しました。"
+            ][locale]);
             s3.output.e.value = res;
             s3.show();
         });
         s2.dialogue = s2.add(new DialogueBanner);
         let s3 = this.add(new UISection);
-        s3.add("h3",0,"4. 暗号の内容が表示されます");
+        s3.add("h3",0,[
+            "4. Decrypted contents will be displayed below",
+            "4. 暗号の内容が表示されます"
+        ][locale]);
         s3.output = s3.add(new TextArea(true));
         generate();
     }
 }
 
+let localeCodeMap = {
+    EN: 0,
+    JP: 1
+};
+let localeCodeMapReverse = ["EN","JP"];
 
 (async ()=>{
-    let locale = URL.query.lang || "EN";
+    locale = localeCodeMap[(SearchBar.query.lang || "EN").trim().toUpperCase()];
     let header = body.add("header");
+    let nav = header.add("nav");
+    nav.add("div",0,0,"flex:1;");
+    nav.add("span",0,"🌐","margin-right:0.3em;");
+    let langsel = nav.add(new Selector("English","日本語"));
+    langsel.selectIndex(locale,true);
+    langsel.on("input",(locale)=>{
+        let code = localeCodeMapReverse[locale];
+        SearchBar.query.lang = code;
+        SearchBar.reload();
+    });
     let h1 = header.add("h1");
-    h1.add("span",0,"🔐メッセージ","display:inline-block");
-    h1.add("span",0,"暗号化サービス","display:inline-block");
-    header.add("p",0,"手順を踏んでコピペするだけ。YouTubeコメント欄、Discordなどどのサービスでも使えます。");
+    if(locale === 0){
+        h1.add("span",0,"Public Key Message Encrypton Tool🔐","display:inline-block");
+    }else if (locale === 1){
+        h1.add("span",0,"メッセージ","display:inline-block");
+        h1.add("span",0,"暗号化サービス🔐","display:inline-block");
+    }
+    header.add("p",0,[
+        "Encryption simplified.<br>Copy and paste your keys to encrypt your messages on YouTube, Discord, and more.",
+        "手順を踏んでコピペするだけ。YouTubeコメント欄、Discordなどどのサービスでも使えます。"
+    ][locale]);
     let ul = header.add("ul",0,0,"max-width:500px;margin:0 auto;");
-    ul.add("h2",0,"注意事項");
-    ul.add("li",0,"送信側はリロードするとセキュリティーのため古い鍵が消去されるので、タブを開けたままにしてください。","font-size:1rem;text-align:left;");
-    ul.add("li",0,"スマホでも使えますがPCをお勧めします。","font-size:1rem;text-align:left;");
+    ul.add("h2",0,["Warning","注意事項"][locale]);
+    ul.add("li",0,[
+        "Keys will be deleted every time the page refreshes for a security reason. Please keep the tab open throughout all the four steps.",
+        "送信側はリロードするとセキュリティーのため古い鍵が消去されるので、タブを開けたままにしてください。"
+    ][locale],"font-size:1rem;text-align:left;");
+    ul.add("li",0,[
+        "This site can be used on a smartphone, but it is more optimized for a use on PC.",
+        "スマホでも使えますがPCをお勧めします。"
+    ][locale],"font-size:1rem;text-align:left;");
     //this.add("h2",0,"↓スクロールする↓");
     let content = body.add("article","class:content");
     //content.add("h2",0,"メッセージを[送りたい|受け取りたい]");
     let s1 = content.add("section");
-    let h3 = s1.add("h3",0,"1. 送信側/受信側を選択");
-    let h2 = s1.add("h4",0,"メッセージを");
-    let select = h2.add(new Selector("送りたい","受け取りたい"));
+    let h3 = s1.add("h3",0,[
+        "1. Please select between sending and receiving",
+        "1. 送信側/受信側を選択"
+    ][locale]);
+    let h2 = s1.add("h4",0,["I'd like to ","メッセージを"][locale]);
+    let select = h2.add(new Selector(["send","送りたい"][locale],["receive","受け取りたい"][locale]));
     let sgui = content.add(new SenderGUI);
     let rgui = content.add(new ReceiverGUI);
     let guis = [sgui,rgui];
